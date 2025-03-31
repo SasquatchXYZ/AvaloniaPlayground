4a11e2a14838724cb5099592c4ec7094e7769175
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2025-03-30 22:32:04 -0400

    Initial commit

---
+++ SimpleToDoList/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace SimpleToDoList.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    /// <summary>
+    /// Gets a collection of <see cref="Models.ToDoItem"/> which allows adding and removing items
+    /// </summary>
+    public ObservableCollection<ToDoItemViewModel> ToDoItems { get; } = [];
+
+    /// <summary>
+    /// Gets or set the content for new Items to add.  If this string is not empty, the AddItemCommand will be enabled automatically
+    /// </summary>
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(AddItemCommand))] // This attribute will invalidate the command each time this property changes
+    private string? _newItemContent;
+
+    /// <summary>
+    /// Returns if a new Item can be added.  We require to have hte NewItem some Text
+    /// </summary>
+    private bool CanAddItem() => !string.IsNullOrWhiteSpace(NewItemContent);
+
+    /// <summary>
+    /// This command is used to add a new Item to the List
+    /// </summary>
+    [RelayCommand (CanExecute = nameof(CanAddItem))]
+    private void AddItem()
+    {
+        // Add a new Item to the List
+        ToDoItems.Add(new ToDoItemViewModel { Content = NewItemContent });
+
+        // Reset the NewItemContent
+        NewItemContent = null;
+    }
+
+    /// <summary>
+    /// Removes the given Item from the List
+    /// </summary>
+    [RelayCommand]
+    private void RemoveItem(ToDoItemViewModel item)
+    {
+        // Remove the given item from the list
+        ToDoItems.Remove(item);
+    }
+}
