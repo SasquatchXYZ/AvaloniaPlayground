ee90abf3d76b2306ff27d150fabc9198765a12db
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2025-04-01 23:18:42 -0400

    Mock Search
    - Reactive View Model

--- Avalonia.MusicStore/ViewModels/MusicStoreViewModel.cs
+++ Avalonia.MusicStore/ViewModels/MusicStoreViewModel.cs
@@ -1,6 +1,21 @@
+using ReactiveUI;
+
 namespace Avalonia.MusicStore.ViewModels;
 
-public class MusicStoreViewModel
+public class MusicStoreViewModel : ViewModelBase
 {
-    
+    private string? _searchText;
+    private bool _isBusy;
+
+    public string? SearchText
+    {
+        get => _searchText;
+        set => this.RaiseAndSetIfChanged(ref _searchText, value);
+    }
+
+    public bool IsBusy
+    {
+        get => _isBusy;
+        set => this.RaiseAndSetIfChanged(ref _isBusy, value);
+    }
 }
