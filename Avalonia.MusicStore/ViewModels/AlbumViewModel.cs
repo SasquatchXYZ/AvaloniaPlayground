14bbd8eaf3c98e03a99d42a70fcf66aa31ddbf42
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2025-04-02 22:58:32 -0400

    Displaying Images
    - Album View Model

--- Avalonia.MusicStore/ViewModels/AlbumViewModel.cs
+++ Avalonia.MusicStore/ViewModels/AlbumViewModel.cs
@@ -1,10 +1,15 @@
+using System.Threading.Tasks;
+using Avalonia.Media.Imaging;
 using Avalonia.MusicStore.Models;
+using ReactiveUI;
 
 namespace Avalonia.MusicStore.ViewModels;
 
 public class AlbumViewModel : ViewModelBase
 {
     private readonly Album _album;
+
+    private Bitmap? _cover;
 
     public AlbumViewModel(Album album)
     {
@@ -14,4 +19,18 @@ public class AlbumViewModel : ViewModelBase
     public string Artist => _album.Artist;
 
     public string Title => _album.Title;
+
+    public Bitmap? Cover
+    {
+        get => _cover;
+        private set => this.RaiseAndSetIfChanged(ref _cover, value);
+    }
+
+    public async Task LoadCover()
+    {
+        await using (var imageStream = await _album.LoadCoverBitmapAsync())
+        {
+            Cover = await Task.Run(() => Bitmap.DecodeToWidth(imageStream, 400));
+        }
+    }
 }
