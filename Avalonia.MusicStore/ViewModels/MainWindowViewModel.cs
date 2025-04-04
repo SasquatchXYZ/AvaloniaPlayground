fb42cf8d42c4ad4c7bff1543b6f35d49c1e46ae1
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2025-04-03 22:52:56 -0400

    Load Data at Start-up

--- Avalonia.MusicStore/ViewModels/MainWindowViewModel.cs
+++ Avalonia.MusicStore/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reactive.Concurrency;
 using System.Reactive.Linq;
 using System.Windows.Input;
+using Avalonia.MusicStore.Models;
 using ReactiveUI;
 
 namespace Avalonia.MusicStore.ViewModels;
@@ -9,6 +12,8 @@ public class MainWindowViewModel : ViewModelBase
 {
     public MainWindowViewModel()
     {
+        RxApp.MainThreadScheduler.Schedule(LoadAlbums);
+
         ShowDialog = new Interaction<MusicStoreViewModel, AlbumViewModel?>();
 
         BuyMusicCommand = ReactiveCommand.CreateFromTask(async () =>
@@ -26,4 +31,24 @@ public class MainWindowViewModel : ViewModelBase
     public ICommand BuyMusicCommand { get; }
     public Interaction<MusicStoreViewModel, AlbumViewModel?> ShowDialog { get; }
     public ObservableCollection<AlbumViewModel> Albums { get; } = [];
+
+    private async void LoadAlbums()
+    {
+        // Loads the list of albums from the disk cache, then transforms each data model (`Album`)
+        // into a view model (`AlbumViewModel`).
+        var albums = (await Album.LoadCachedAsync()).Select(x => new AlbumViewModel(x));
+
+        // Adds the AlbumViewModels to the ObservableCollection (updating the UI)
+        foreach (var album in albums)
+        {
+            Albums.Add(album);
+        }
+
+        // Cover art is loaded asynchronously - ensuring the app remains responsive
+        // during the image loading process.
+        foreach (var album in Albums.ToList())
+        {
+            await album.LoadCover();
+        }
+    }
 }
